44d9da246ee9a9a1e28b1e12b25f5ae11abbc542
leonraz58 <leonraz58@gmail.com>
2025-04-30 16:58:53 +0300

    v-on

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
-  Чпэк
+  <div>
+    <div>
+      <button v-on:click="addLike">Like</button>
+      <button @click="addDislike">Dislike</button>
+<!--      два варианта записи v-on-->
+    </div>
+    <div><strong>Количество лайков {{ likes }}</strong> </div>
+    <div><strong>Количество лайков {{ dislikes }}</strong> </div>
+  </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1
+    },
+    addDislike() {
+      this.dislikes += 1
+    }
+  }
+}
 </script>
 
 <style>
