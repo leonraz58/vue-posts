d36b30a79fbdb520e303d68cc152c8caa692a5a3
leonraz58 <leonraz58@gmail.com>
2025-04-30 17:38:52 +0300

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <div>
-      <button v-on:click="addLike">Like</button>
-      <button @click="addDislike">Dislike</button>
-<!--      два варианта записи v-on-->
+  <div class="app">
+    <form>
+      <h4>Создание поста</h4>
+      <input type="text" class="input" placeholder="Название">
+      <input type="text" class="input" placeholder="Описание">
+      <button class="btn">Создать</button>
+    </form>
+
+    <div class="post" v-for="post in posts">
+      <div><strong>Название: </strong> {{ post.title }}</div>
+      <div><strong>Описание: </strong> {{ post.body }}</div>
     </div>
-    <div><strong>Количество лайков {{ likes }}</strong> </div>
-    <div><strong>Количество лайков {{ dislikes }}</strong> </div>
   </div>
 </template>
 
@@ -14,21 +18,52 @@
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 0
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'Описание поста'},
+        {id: 2, title: 'JavaScript', body: 'Описание поста 2'},
+        {id: 3, title: 'JavaScript', body: 'Описание поста 3'},
+      ]
     }
   },
-  methods: {
-    addLike() {
-      this.likes += 1
-    },
-    addDislike() {
-      this.dislikes += 1
-    }
-  }
+  methods: {}
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+.app {
+  padding: 20px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
 </style>
