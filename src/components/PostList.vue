56d593bcbf5c576508593f2795c764eeb73ed732
leonraz58 <leonraz58@gmail.com>
2025-05-03 01:11:56 +0300

    TransitionGroup

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,13 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список постов</h3>
-
+  <TransitionGroup name="list">
   <PostItem v-for="post in posts"
             :post="post"
             :key="post.id"
             @remove="$emit('remove', post)"
   />
+  </TransitionGroup>
   </div>
   <h2 v-else>
     Список постов пуст
@@ -29,5 +30,16 @@ export default {
 </script>
 
 <style scoped>
-
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.4s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.list-move {
+  transition: transform 0.4s ease;
+}
 </style>
