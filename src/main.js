c0caa2a18bc0a1b7c49d609a0a2c2d2dc779a365
Leonid Razzhivin <leonraz58@gmail.com>
2025-05-14 15:47:37 +0300

    custom directives

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import components from '@/components/UI'
 import router from "@/router/router";
+import Vintersection from "@/directives/Vintersection";
 
 const app = createApp(App)
 
 components.forEach(component => {
     app.component(component.name, component)
 })
+
+app.directive('intersection', Vintersection)
 
 app .use(router)
     .mount('#app')
--- src/pages/PostsPage.vue
+++ src/pages/PostsPage.vue
@@ -21,7 +21,7 @@
               v-if="!isPostsLoading"
     />
     <div v-else>Идет загрузка...</div>
-    <div ref="observer" class="observer"></div>
+    <div v-intersection="loadMorePosts" class="observer"></div>
     <!--    <div class="page__wrapper">-->
     <!--      <div v-for="pageNumber in totalPages"-->
     <!--           :key="pageNumber"-->
@@ -119,18 +119,6 @@ export default {
   },
   mounted() {
     this.fetchPosts();
-    const options = {
-      rootMargin: "0px",
-      threshold: 1.0,
-    };
-
-    const callback = (entries, observer) => {
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts();
-      }
-    }
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(this.$refs.observer)
   },
   computed: {
     sortedPosts() {
